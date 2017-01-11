c32f94fdee51ee76bd9c7557095e830fc13e3e3a
silence940109 <695412269@qq.com>
2017-01-11 11:48:14 +0800

    UserRepository

---
+++ src/main/scala/com/silence/repository/UserRepository.scala
@@ -0,0 +1,13 @@
+package com.silence.repository
+
+import com.silence.enties.User
+import org.springframework.data.jpa.repository.JpaRepository
+import java.lang.Long
+
+/**
+ * @description User Dao
+ * @date 2017年1月11日
+ * @author wang
+ *
+ */
+trait UserRepository extends JpaRepository[User, Long]
