c300ece38a422c439e35098b763afd5aef461fff
silence940109 <695412269@qq.com>
2017-03-28 21:53:09 +0800

    Start Application

--- src/main/scala/com/silence/Application.scala
+++ src/main/scala/com/silence/Application.scala
@@ -1,5 +1,22 @@
 package com.silence
 
-class Application {
-  
+import org.springframework.context.annotation.Configuration
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+//启动swagger注解
+@EnableSwagger2
+class Config
+
+object Application extends App {
+
+    SpringApplication.run(classOf[Config])
+    
 }
