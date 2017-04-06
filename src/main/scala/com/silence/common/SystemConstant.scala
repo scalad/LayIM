efb859bdae71b5997102a4993f4644c445a6099d
silence940109 <695412269@qq.com>
2017-04-06 18:50:18 +0800

    system constant

--- src/main/scala/com/silence/common/SystemConstant.scala
+++ src/main/scala/com/silence/common/SystemConstant.scala
@@ -9,5 +9,13 @@ object SystemConstant {
     final val SUCCESS_MESSAGE: String = "请求数据成功"
     
     final val SUCCESS_ERROR: String = "请求数据失败"
+    
+    final val UPLOAD_SUCCESS: String = "上传成功"
+    
+    final val UPLOAD_FAIL: String = "上传失败"
+    
+    final val IMAGE_PATH: String = "/upload/image/"
+    
+    final val FILE_PATH: String = "/upload/file/"
   
 }
