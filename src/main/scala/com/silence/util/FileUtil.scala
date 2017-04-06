92f89939611ccffc10319bb82d65fa47c447b5d4
silence940109 <695412269@qq.com>
2017-04-06 18:49:42 +0800

    apache common fileutil upload file

---
+++ src/main/scala/com/silence/util/FileUtil.scala
@@ -0,0 +1,31 @@
+package com.silence.util
+
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import org.apache.commons.io.FileUtils
+import com.silence.common.SystemConstant
+
+/**
+ * @description 服务器文件工具
+ * @date 2017-04-06
+ * @author silence
+ */
+object FileUtil {
+  
+    /**
+     * @description 服务器保存我呢见
+     * @param path 文件绝对路径地址
+     * @param file 二进制文件
+     * @return 文件的相对路径地址
+     */
+    def upload(types: String, path: String, file: MultipartFile) : String = {
+        val name = file.getOriginalFilename
+        //重命名图片的名称
+        val fileName = UUIDUtil.getUUID32String() + name.substring(name.indexOf("."))
+    		//图片保存到服务器
+        val paths = path + DateUtil.getDateString()
+        FileUtils.copyInputStreamToFile(file.getInputStream, new File(paths, fileName))
+        types + DateUtil.getDateString() + "/" + fileName        
+    }
+  
+}
