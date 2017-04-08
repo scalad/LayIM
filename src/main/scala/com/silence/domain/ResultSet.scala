0a509bbc2f6115346d04ab6e370932e89f7c8372
silence940109 <695412269@qq.com>
2017-04-08 11:54:41 +0800

    update the result set

--- src/main/scala/com/silence/domain/ResultSet.scala
+++ src/main/scala/com/silence/domain/ResultSet.scala
@@ -7,13 +7,13 @@ import com.silence.common.SystemConstant
  * @param code 状态，0表示成功，其他表示失败
  * @param msg 额外信息
  */
-class ResultSet[T](var code: Int, var msg: String) {
-        
+class ResultSet[T](var code: Int = SystemConstant.SUCCESS, var msg: String = SystemConstant.SUCCESS_MESSAGE) {
+    
     private var data: T = _  
   
     def this(data: T) = {
-        this(SystemConstant.SUCCESS, SystemConstant.SUCCESS_MESSAGE)
+        this
         this.data = data      
     }
-
+    
 }
