0c8ea8c4447477a9f2a2d58d93c4bc2d1802bd95
silence940109 <695412269@qq.com>
2017-04-08 18:53:59 +0800

    apache common file util

--- src/main/scala/com/silence/util/FileUtil.scala
+++ src/main/scala/com/silence/util/FileUtil.scala
@@ -13,17 +13,27 @@ import com.silence.common.SystemConstant
 object FileUtil {
   
     /**
-     * @description 服务器保存我呢见
+     * @description 服务器保存服务器
+     * @param types 文件类型/upload/image 或  /upload/file
      * @param path 文件绝对路径地址
      * @param file 二进制文件
      * @return 文件的相对路径地址
      */
     def upload(types: String, path: String, file: MultipartFile) : String = {
-        val name = file.getOriginalFilename
-    		//图片保存到服务器
-        val paths = path + DateUtil.getDateString()
+        var name = file.getOriginalFilename
+        var paths = path + types + DateUtil.getDateString
+        var result = types + DateUtil.getDateString + "/"
+        //如果是图片，则使用uuid重命名图片
+        if (SystemConstant.IMAGE_PATH.equals(types)) {
+            name = UUIDUtil.getUUID32String() + name.substring(name.indexOf(".")) 
+        } else if (SystemConstant.FILE_PATH.equals(types)) {
+            //如果是文件，则区分目录
+            val p = UUIDUtil.getUUID32String
+            paths = paths + p
+            result += p + "/"
+        }
         FileUtils.copyInputStreamToFile(file.getInputStream, new File(paths, name))
-        types + DateUtil.getDateString() + "/" + name
+        result + name
     }
   
 }
