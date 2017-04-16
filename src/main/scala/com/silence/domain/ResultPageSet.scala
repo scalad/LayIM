ca9d6cdef5beae2d42365b255569ca7168ee3dbe
silence940109 <695412269@qq.com>
2017-04-16 10:26:55 +0800

    resultPageSet common

--- src/main/scala/com/silence/domain/ResultPageSet.scala
+++ src/main/scala/com/silence/domain/ResultPageSet.scala
@@ -2,6 +2,11 @@ package com.silence.domain
 
 import scala.beans.BeanProperty
 
+/**
+ * @description 分页结果集
+ * @date 2017-04-16
+ * @author silence
+ */
 class ResultPageSet[T] extends ResultSet[T]{
   
     @BeanProperty var pages: Int = _
